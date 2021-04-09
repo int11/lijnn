7530e46b5db98aeb645c64c368ef298191dc6820
kay <injea1291@gmail.com>
2021-04-09 18:54:22 +0900

    linear with numerical differentiation

--- 1.py
+++ 1.py
@@ -1,15 +1,29 @@
 import numpy as np
 
-def softmax(a):
-    exp_a = np.exp(a)
-    sum_exp_a = np.sum(exp_a)
-    y = exp_a / sum_exp_a
+x_train = np.array([1., 2., 3., 4., 5., 6.])
+y_train = np.array([9., 16., 23., 30., 37., 44.])
 
-    return y
+W = 0.0
+b = 0.0
 
-a = np.array([0.3,2.9,4.0])
-exp_a = np.exp(a)
-sum_exp_a = np.sum(exp_a)
-y = exp_a / sum_exp_a
+n_data = len(x_train)
 
-print(a)
+epochs = 5000
+learning_rate = 0.01
+
+for i in range(epochs):
+    hypothesis = x_train * W + b
+    cost = np.sum((hypothesis - y_train) ** 2) / n_data
+    gradient_w = np.sum((W * x_train - y_train + b) * 2 * x_train) / n_data
+    gradient_b = np.sum((W * x_train - y_train + b) * 2) / n_data
+
+    W -= learning_rate * gradient_w
+    b -= learning_rate * gradient_b
+
+    if i % 100 == 0:
+        print('Epoch ({:10d}/{:10d}) cost: {:10f}, W: {:10f}, b:{:10f}'.format(i, epochs, cost, W, b))
+
+print('W: {:10f}'.format(W))
+print('b: {:10f}'.format(b))
+print('result : ')
+print(x_train * W + b)
--- main.py
+++ main.py
@@ -1,15 +1,40 @@
-def numerical_diff(dxindex, f, *x):
+import numpy as np
+
+
+def numerical_diff(dxindex, f, w):
     h = 1e-4  # 0.0001
-    x1, x2 = list(x), list(x)
+    x1, x2 = list(w), list(w)
     x1[dxindex] += h
     x2[dxindex] -= h
-    return (f(*x1) - f(*x2)) / 2 * h
+    return (f(x1) - f(x2)) / 2 * h
 
 
-def function_temp1(*x):
-    return x[0] * x[0] + x[1] * x[1]
+def linear(w):
+    return w[0] * datax + w[1]
 
 
-x = [3.0, 4.0]
-print(numerical_diff(0, function_temp1, *x))
-print(numerical_diff(1, function_temp1, *x))
+def MSEmaker(f):
+    def MSE(w):
+        return np.sum((datay - f(w)) ** 2) / lenx
+
+    return MSE
+
+
+datax = np.array([1., 2., 3., 4., 5., 6.])
+datay = np.array([9., 16., 23., 30., 37., 44.])
+lenx = len(datax)
+
+w = [0.,0.]
+learning_rate = 10000
+
+MSE = MSEmaker(linear)
+gradient = [0, 0]
+
+for i in range(500000):
+    cost = MSE(w)
+    gradient[0] = numerical_diff(0, MSE, w)
+    gradient[1] = numerical_diff(1, MSE, w)
+    w[0] -= learning_rate * gradient[0]
+    w[1] -= learning_rate * gradient[1]
+    if i % 100 == 0:
+        print('Epoch ({:10d}/{:10d}) cost: {:10f}, W: {:10f}, b:{:10f}'.format(i, 500, cost, w[0], w[1]))

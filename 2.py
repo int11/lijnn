36146897dbec5fe5abc8c78736725f9f0c821b0e
string <43224413+int11@users.noreply.github.com>
2021-06-05 20:33:00 +0900

    class

--- 2.py
+++ 2.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class nn:
-    def __init__(self, x, y):
+    def __init__(self, x, y, actifuns, costfuns, ):
         self.h = 1e-4  # 0.0001
         self.learning_rate = 1e-2
         self.x = x
@@ -13,24 +13,33 @@ class nn:
         self.w = np.zeros(len(self.x))
         self.b = np.zeros(1)
 
+        if actifuns == "linear":
+            def activation_fun():
+                return np.matmul(self.w, self.x) + self.b
+        elif actifuns == "sigmoid":
+            def activation_fun():
+                return 1 / (1 + np.exp(-(np.matmul(self.w, self.x) + self.b)))
+        else:
+            raise
+        self.activation_fun = activation_fun
+
+        if costfuns == "mse":
+            def cost_fun():
+                return np.sum((self.activation_fun() - self.y) ** 2) / self.lensample
+        elif costfuns == "binary_crossentropy":
+            def cost_fun():
+                delta = 1e-7
+                return -np.sum(self.y * np.log(self.activation_fun() + delta) + (1 - self.y) * np.log(
+                    (1 - self.activation_fun()) + delta))
+        elif costfuns == "categorical_crossentropy":
+            raise
+        else:
+            raise
+        self.cost_fun = cost_fun
+
     def __call__(self):
-
-        def f():
-            hypothesis = np.matmul(self.w, self.x) + self.b
-            return self.cross_entropy(self.sigmoid(hypothesis))
-
-        self.numerical_diff(f, self.w)
-        self.numerical_diff(f, self.b)
-
-    def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
-
-    def MSE(self, hypothesis):
-        return np.sum((hypothesis - self.y) ** 2) / self.lensample
-
-    def cross_entropy(self, hypothesis):
-        delta = 1e-7
-        return -np.sum(self.y * np.log(hypothesis + delta) + (1 - self.y) * np.log((1 - hypothesis) + delta))
+        self.numerical_diff(self.cost_fun, self.w)
+        self.numerical_diff(self.cost_fun, self.b)
 
     def numerical_diff(self, f, x):
         for i in range(len(x)):
@@ -43,12 +52,13 @@ class nn:
             x[i] -= self.learning_rate * (f1 - f2) / (2 * self.h)
 
 
-x = np.array([[2, 4], [4, 11], [6, 6], [8, 5], [10, 7], [12, 16], [14, 8], [16, 3], [18, 7]])
-y = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1])
-nn = nn(x.T, y)
+x = np.array([[4.9,3.0,1.4,0.2],[5.8,2.6,4.0,1.2],[6.7,3.0,5.2,2.3],[5.6,2.8,4.9,2.0]])
+y = np.array(["setosa","setosa","versicolor","virginica","virginica"])
+nn = nn(x.T, y, "sigmoid", "binary_crossentropy")
 for i in range(800000):
     nn()
     if i % 10000 == 0:
-        print(nn.w, nn.b)
+        print(nn.cost_fun(), nn.w, nn.b)
+x = [[5.1,3.5,1.4,0.2]
 
-print(nn.sigmoid(np.matmul(nn.w, nn.x) + nn.b))
+print(nn.activation_fun())

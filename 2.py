6e0ae8d60299cab9b28baa1d7b007782b2d057ec
string <43224413+int11@users.noreply.github.com>
2021-06-09 18:01:04 +0900

    softmax regression

--- 2.py
+++ 2.py
@@ -2,16 +2,17 @@ import numpy as np
 
 
 class nn:
-    def __init__(self, x, y, actifuns, costfuns, ):
+    def __init__(self, x, y, actifuns, costfuns, xlen, ylen):
         self.h = 1e-4  # 0.0001
+        self.delta = 1e-7
         self.learning_rate = 1e-2
         self.x = x
-        self.y = y
-        if len(self.x.shape) == 1:
+        self.y = oneshotencoding(y).T
+        if self.x.ndim == 1:
             self.x = self.x[np.newaxis]
         self.lensample = len(self.x[0])
-        self.w = np.zeros(len(self.x))
-        self.b = np.zeros(1)
+        self.w = np.zeros((ylen, xlen)).reshape(ylen, xlen)
+        self.b = np.zeros((ylen, 1)).reshape(ylen, 1)
 
         if actifuns == "linear":
             def activation_fun():
@@ -28,11 +29,11 @@ class nn:
                 return np.sum((self.activation_fun() - self.y) ** 2) / self.lensample
         elif costfuns == "binary_crossentropy":
             def cost_fun():
-                delta = 1e-7
-                return -np.sum(self.y * np.log(self.activation_fun() + delta) + (1 - self.y) * np.log(
-                    (1 - self.activation_fun()) + delta))
+                return -np.sum(self.y * np.log(self.activation_fun() + self.delta) + (1 - self.y) * np.log(
+                    (1 - self.activation_fun()) + self.delta))
         elif costfuns == "categorical_crossentropy":
-            raise
+            def cost_fun():
+                return -np.sum(self.y * np.log(self.activation_fun() + self.delta))/self.lensample
         else:
             raise
         self.cost_fun = cost_fun
@@ -42,23 +43,31 @@ class nn:
         self.numerical_diff(self.cost_fun, self.b)
 
     def numerical_diff(self, f, x):
-        for i in range(len(x)):
-            args = x[i]
-            x[i] = args + self.h
+        for i in range(x.size):
+            args = x.reshape(-1)[i]
+            x.reshape(-1)[i] = args + self.h
+            print(x)
+            print(f())
+            print(self.activation_fun())
             f1 = f()
-            x[i] = args - self.h
+            x.reshape(-1)[i] = args - self.h
+            print(x)
+            print(f())
+            print(self.activation_fun())
             f2 = f()
-            x[i] = args
-            x[i] -= self.learning_rate * (f1 - f2) / (2 * self.h)
+            x.reshape(-1)[i] = args
+            print((f1 - f2) / (2 * self.h))
+            x.reshape(-1)[i] -= self.learning_rate * (f1 - f2) / (2 * self.h)
+
+def oneshotencoding(data):
+    return np.eye(np.max(data) + 1)[data]
 
 
-x = np.array([[4.9,3.0,1.4,0.2],[5.8,2.6,4.0,1.2],[6.7,3.0,5.2,2.3],[5.6,2.8,4.9,2.0]])
-y = np.array(["setosa","setosa","versicolor","virginica","virginica"])
-nn = nn(x.T, y, "sigmoid", "binary_crossentropy")
-for i in range(800000):
+x = np.array([[5.1,3.5,1.4,0.2],[4.9, 3.0, 1.4, 0.2], [5.8, 2.6, 4.0, 1.2], [6.7, 3.0, 5.2, 2.3], [5.6, 2.8, 4.9, 2.0]])
+y = np.array([0, 0, 1, 2, 2])
+nn = nn(x.T, y, "sigmoid", "categorical_crossentropy", 4, 3)
+for i in range(1):
     nn()
-    if i % 10000 == 0:
-        print(nn.cost_fun(), nn.w, nn.b)
-x = [[5.1,3.5,1.4,0.2]
+    print(nn.cost_fun(), nn.w, nn.b)
 
 print(nn.activation_fun())

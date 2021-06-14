1b8666afb84e9ffa9b60ac5a6fe9dd3658f71e7e
kay <injea1291@gmail.com>
2021-06-14 23:42:38 +0900

    softmax

--- 2.py
+++ 2.py
@@ -7,19 +7,19 @@ class nn:
         self.delta = 1e-7
         self.learning_rate = 1e-2
         self.x = x
-        self.y = oneshotencoding(y).T
+        self.y = oneshotencoding(y)
         if self.x.ndim == 1:
             self.x = self.x[np.newaxis]
         self.lensample = len(self.x[0])
-        self.w = np.zeros((ylen, xlen)).reshape(ylen, xlen)
-        self.b = np.zeros((ylen, 1)).reshape(ylen, 1)
+        self.w = np.zeros(( xlen,ylen))
+        self.b = np.zeros((1,ylen))
 
         if actifuns == "linear":
             def activation_fun():
-                return np.matmul(self.w, self.x) + self.b
+                return np.matmul(self.x,self.w) + self.b
         elif actifuns == "sigmoid":
             def activation_fun():
-                return 1 / (1 + np.exp(-(np.matmul(self.w, self.x) + self.b)))
+                return 1 / (1 + np.exp(-(np.matmul(self.x,self.w) + self.b)))
         else:
             raise
         self.activation_fun = activation_fun
@@ -33,7 +33,7 @@ class nn:
                     (1 - self.activation_fun()) + self.delta))
         elif costfuns == "categorical_crossentropy":
             def cost_fun():
-                return -np.sum(self.y * np.log(self.activation_fun() + self.delta))/self.lensample
+                return -np.sum(self.y * np.log(self.activation_fun() + self.delta)) / self.lensample
         else:
             raise
         self.cost_fun = cost_fun
@@ -59,15 +59,27 @@ class nn:
             print((f1 - f2) / (2 * self.h))
             x.reshape(-1)[i] -= self.learning_rate * (f1 - f2) / (2 * self.h)
 
+
 def oneshotencoding(data):
     return np.eye(np.max(data) + 1)[data]
 
 
-x = np.array([[5.1,3.5,1.4,0.2],[4.9, 3.0, 1.4, 0.2], [5.8, 2.6, 4.0, 1.2], [6.7, 3.0, 5.2, 2.3], [5.6, 2.8, 4.9, 2.0]])
+x = np.array(
+    [[5.1, 3.5, 1.4, 0.2], [4.9, 3.0, 1.4, 0.2], [5.8, 2.6, 4.0, 1.2], [6.7, 3.0, 5.2, 2.3], [5.6, 2.8, 4.9, 2.0]])
 y = np.array([0, 0, 1, 2, 2])
-nn = nn(x.T, y, "sigmoid", "categorical_crossentropy", 4, 3)
-for i in range(1):
+nn = nn(x, y, "sigmoid", "categorical_crossentropy", 4, 3)
+
+for i in range(1000):
     nn()
     print(nn.cost_fun(), nn.w, nn.b)
 
-print(nn.activation_fun())
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a-c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+for i in nn.activation_fun():
+    print(softmax(i))
+

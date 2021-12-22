19941bf6b0df4d7f38072ee4253adb2a55cc33cc
string <43224413+int11@users.noreply.github.com>
2021-12-22 10:30:48 +0900

    branch

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from model import *
 from dataset.mnist import load_mnist
 
 (x, t), (x_test, t_test) = load_mnist(flatten=False)
-print(x.shape,x_test.shape)
+
 x1 = np.array(
     [[5.1, 3.5, 1.4, 0.2], [4.9, 3.0, 1.4, 0.2], [4.7, 3.2, 1.3, 0.2], [4.6, 3.1, 1.5, 0.2], [5.0, 3.6, 1.4, 0.2],
      [5.4, 3.9, 1.7, 0.4], [4.6, 3.4, 1.4, 0.3], [5.0, 3.4, 1.5, 0.2], [4.4, 2.9, 1.4, 0.2], [4.9, 3.1, 1.5, 0.1],

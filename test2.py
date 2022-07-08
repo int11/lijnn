54644451feb1bdfa21174b2ad195883936d068d3
kay <injea1291@gmail.com>
2022-07-08 23:30:59 +0900

    copy

--- test2.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-x = np.array([2, 5, 10, 20, 30, 40, 50])
-w = np.array([3, 4, 10, 20, 30, 40, 50]).reshape(1,-1)
-if x.ndim == 1: x = x[np.newaxis].T
-
-print(x,'\n',w)
-print(np.dot(x, w))
-print(x*w)

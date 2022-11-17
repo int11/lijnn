d520521bec29f1504782b0c78b1a86e874261570
surfacepro <injea1291@gmail.com>
2022-11-17 17:29:32 +0900

    Fix R-CNN, add Fast-R-CNN

--- example/CNN/fastrcnn.py
+++ example/CNN/fastrcnn.py
@@ -2,6 +2,8 @@ from lijnn import *
 from lijnn import layers as L
 from lijnn import functions as F
 from lijnn.transforms import *
+from example.CNN import VGG16
 
 
 class Fast_R_CNN(Model):
+    pass

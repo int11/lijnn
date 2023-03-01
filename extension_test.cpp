2a6903a76e4446beaa2494664d1266824a836365
injea <injea1291@gmail.com>
2023-03-02 00:35:24 +0900

    slightly change

--- extension_test.cpp
+++ extension_test.cpp
@@ -11,18 +11,33 @@ double cosh_impl(double x) {
     return (1 + pow(e, (-2 * x))) / (2 * pow(e, -x));
 }
 
-PyObject* tanh_impl(PyObject*, PyObject* o) {
+PyObject* CPython_tanh_impl(PyObject*, PyObject* o) {
     double x = PyFloat_AsDouble(o);
     double tanh_x = sinh_impl(x) / cosh_impl(x);
     return PyFloat_FromDouble(tanh_x);
 }
 
+PyObject* CPython_tanh_impl_point(PyObject*, PyObject* o) {
+    Py_ssize_t tot_len = PyList_Size(o);
+    PyObject* ret = PyList_New(tot_len);
+
+    for (int i = 0; i < tot_len; i++) {
+        double x = PyFloat_AsDouble(PyList_GetItem(o, i));
+        double tanh_x = sinh_impl(x) / cosh_impl(x);
+        PyList_SetItem(ret, i, PyFloat_FromDouble(tanh_x));
+    }
+
+    Py_INCREF(ret);
+
+    return ret;
+}
 
 static PyMethodDef superfastcode_methods[] = {
     // The first property is the name exposed to Python, fast_tanh
     // The second is the C++ function with the implementation
     // METH_O means it takes a single PyObject argument
-    { "fast_tanh", (PyCFunction)tanh_impl, METH_O, "asdfasdftestete" },
+    { "CPython_tanh_impl", (PyCFunction)CPython_tanh_impl, METH_O, "asdfasdftestete" },
+    { "CPython_tanh_impl_point", (PyCFunction)CPython_tanh_impl_point, METH_O, "asdfasdftestete" },
 
     // Terminate the array with an object containing nulls.
     { nullptr, nullptr, 0, nullptr }
--- main_c_extension.py
+++ main_c_extension.py
@@ -3,13 +3,14 @@ sys.path.append(os.getcwd())
 
 from random import random
 from time import perf_counter
-from extension_test import fast_tanh
+from extension_test import CPython_tanh_impl, CPython_tanh_impl_point
 import ctypes
 import sys, os
 from array import array
+import numpy as np
+
 libc = ctypes.cdll.LoadLibrary(os.path.dirname(__file__) + '/ctypes_test.so')
-print(libc)
-COUNT = 5000000  # Change this value depending on the speed of your computer
+COUNT = 10000000  # Change this value depending on the speed of your computer
 data = [(random() - 0.5) * 3 for _ in range(COUNT)]
 
 e = 2.7182818284590452353602874713527
@@ -30,36 +31,42 @@ def test(fn, name):
     duration = perf_counter() - start
     print(f'{name} took {duration:.3f} seconds\n\n')
 
-    for d in result[:5000000]:
+    for d in result:
         assert -1 <= d <= 1, " incorrect values"
 
 if __name__ == "__main__":
-    tanh_impl = libc.tanh_impl
-    sinh_impl = libc.sinh_impl
-    tanh_impl_point = libc.tanh_impl_point
-    tanh_impl.restype = ctypes.c_double
-    sinh_impl.restype = ctypes.c_double
-    tanh_impl_point.restype = ctypes.POINTER(ctypes.c_double)
+    ctypes_tanh_impl = libc.ctypes_tanh_impl
+    ctypes_tanh_impl_point = libc.ctypes_tanh_impl_point
+    ctypes_tanh_impl.restype = ctypes.c_double
+    ctypes_tanh_impl_point.restype = ctypes.POINTER(ctypes.c_double * COUNT)
 
-    print(data[1], ctypes.c_double(data[1]))
-    print(sinh_impl(ctypes.c_double(data[1])), sinh(data[1]))
-    print()
-    print(tanh(data[1]),fast_tanh(data[1]))
-    print(tanh_impl(ctypes.c_double(data[1])))
+    def ctypes_pointer(data):
+        temp = array('d', data)
+        temp = (ctypes.c_double * len(data)).from_buffer(temp)
+        result = ctypes_tanh_impl_point(temp, len(data))
+        return result[0]
+
+    def ctypes_pointer_numpy(data):
+        temp = np.ctypeslib.as_ctypes(np.array(data))
+        result = ctypes_tanh_impl_point(temp, len(data))
+        result = np.ctypeslib.as_array(result[0])
+        return result
 
     
+    print(ctypes_tanh_impl(ctypes.c_double(data[1])), ctypes_pointer(data)[1], ctypes_pointer_numpy(data)[1])
+    print(CPython_tanh_impl(data[1]), CPython_tanh_impl_point(data)[1])
+    print(tanh(data[1]))
+
 
     print('Running benchmarks with COUNT = {}'.format(COUNT))
 
-    test(lambda d: [tanh(x) for x in d], '[tanh(x) for x in d] (Python implementation)')
-    test(lambda d: [fast_tanh(x) for x in d], '[fast_tanh(x) for x in d] (CPython C++ extension)')
-    test(lambda d: [tanh_impl(ctypes.c_double(x)) for x in d], '[fast_tanh(x) for x in d] (ctypes)')
-
-    def aa(data0):
-        temp = array('d', data0)
-        temp = (ctypes.c_double * len(data0)).from_buffer(temp)
-        return tanh_impl_point(temp, len(data0))
-
-    test(lambda d: aa(data), 'tanh_impl_point')
-
     
+    
+    test(lambda d: [ctypes_tanh_impl(ctypes.c_double(x)) for x in d], 'ctypes')
+    test(lambda d: ctypes_pointer(data), 'ctypes pointer')
+    test(lambda d: ctypes_pointer_numpy(data), 'ctypes pointer numpy')
+
+    test(lambda d: [CPython_tanh_impl(x) for x in d], 'CPython C++ extension')
+    test(lambda d: CPython_tanh_impl_point(data), 'CPython C++ extension pointer')
+    
+    test(lambda d: [tanh(x) for x in d], 'Python implementation')

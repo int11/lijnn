44c03db39c6b08131daa732ffff884bfc2c28109
injea <injea1291@gmail.com>
2023-05-05 00:47:16 +0900

    c

--- cuda_test.cu
+++ cuda_test.cu
@@ -15,35 +15,39 @@ using namespace std ;
 const double e = 2.7182818284590452353602874713527;
 
 
-__global__ void tanh_impl_Kernel(double *d_x, int cnt, double *d_result){
-	int idx =threadIdx.x + blockIdx.x * blockDim.x;
-    double sinh = (1 - pow(e, (-2 * d_x[idx]))) / (2 * pow(e, -d_x[idx]));
-    double cosh = (1 + pow(e, (-2 * d_x[idx]))) / (2 * pow(e, -d_x[idx]));
 
-	d_result[idx] = sinh / cosh;
+__global__ void tanh_impl_Kernel(double * d_in, double * d_out){
+	
+	int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    
+	// double sinh = (1 - pow(e, (-2 * d_in[idx]))) / (2 * pow(e, -d_in[idx]));
+    // double cosh = (1 + pow(e, (-2 * d_in[idx]))) / (2 * pow(e, -d_in[idx]));
+    // d_out[idx] = sinh / cosh;
+    d_out[idx] = ((1 - pow(e, (-2 * d_in[idx]))) / (2 * pow(e, -d_in[idx]))) / ((1 + pow(e, (-2 * d_in[idx]))) / (2 * pow(e, -d_in[idx])));
+
 }
 
-extern "C" double* tanh_impl(double* x, int cnt){
-    double *result =(double *)malloc(cnt);
-    double *d_x, *d_result;
 
-    cudaMalloc(&d_x, cnt);
-    cudaMalloc(&d_result, cnt);
+DELLEXPORT double* tanh_impl(double * h_in, int arr_size){
+	
+	const long long int ARRAY_BYTES = arr_size * sizeof(double);
+    double * h_out = (double *)malloc(arr_size);
+	double *d_in, *d_out;
 
-    cudaMemcpy(d_x, x, cnt, cudaMemcpyHostToDevice);
-    
+	cudaMalloc(&d_in, ARRAY_BYTES);
+	cudaMalloc(&d_out, ARRAY_BYTES);
 
+	cudaMemcpy(d_in, h_in, ARRAY_BYTES, cudaMemcpyHostToDevice);
+	
+	tanh_impl_Kernel<<< 1, arr_size >>>(d_in, d_out);
 
-    tanh_impl_Kernel <<< cnt / THREADS_PER_BLOCK , THREADS_PER_BLOCK >>>(d_x, cnt, d_result); 
+	cudaMemcpy(h_out, d_out, ARRAY_BYTES, cudaMemcpyDeviceToHost);
 
-
-    cudaMemcpy(result, d_result, cnt, cudaMemcpyDeviceToHost); 
-
-    cudaFree(d_x); 
-    cudaFree(d_result); 
-    
-    return result;
+	cudaFree(d_in);
+	cudaFree(d_out);
+    return h_out;
 }
+
 
 // int main()
 // {
@@ -63,31 +67,3 @@ extern "C" double* tanh_impl(double* x, int cnt){
 
 // 	return 0;
 // }
-
-
-__global__ void cudaSquareKernel(float * d_in, float * d_out){
-	
-	int idx = threadIdx.x + blockIdx.x * blockDim.x;
-	printf("threadIdx.x : %d, blockIdx.x : %d, blockDim.x : %d \n", threadIdx.x, blockIdx.x, blockDim.x);
-	d_out[idx] = d_in[idx] * d_in[idx] ;
-}
-
-DELLEXPORT float* cudaSquare(float * h_in, int arr_size){
-	
-	const long long int ARRAY_BYTES = arr_size * sizeof(float) ;
-    float * h_out = (float *)malloc(arr_size);
-	float *d_in, *d_out ;
-
-	cudaMalloc(&d_in, ARRAY_BYTES) ;
-	cudaMalloc(&d_out, ARRAY_BYTES) ;
-
-	cudaMemcpy(d_in, h_in, ARRAY_BYTES, cudaMemcpyHostToDevice) ;
-	
-	cudaSquareKernel<<< 1, arr_size >>>(d_in, d_out) ;
-
-	cudaMemcpy(h_out, d_out, ARRAY_BYTES, cudaMemcpyDeviceToHost) ;
-
-	cudaFree(d_in) ;
-	cudaFree(d_out) ;
-    return h_out;
-}

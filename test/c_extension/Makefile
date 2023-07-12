7d66c2482e668abbe1a655b07da699bf99a34136
injea <injea1291@gmail.com>
2023-07-12 23:46:32 +0900

    C extension

--- test/c_extension/Makefile
+++ test/c_extension/Makefile
@@ -1,4 +1,5 @@
-all: ctypes_test.so cuda_test.so extension_test.so
+INC=-I/usr/include/python3.10 -I/home/injea/lijnn/.venv/lib/python3.10/site-packages/numpy/core/include 
+all: ctypes_test.so cuda_test.so extensiontest.so numpy_extension.so
 
 ctypes_test.so: ctypes_test.cpp
 	g++ -g -shared ctypes_test.cpp -o ctypes_test.so
@@ -6,5 +7,9 @@ ctypes_test.so: ctypes_test.cpp
 cuda_test.so: cuda_test.cu
 	nvcc -Xcompiler -fPIC -shared -g cuda_test.cu -o cuda_test.so
 
-extension_test.so: extension_test.cpp
-	g++ -g -shared extension_test.cpp -o extension_test.so
+extensiontest.so: extensiontest.cpp
+	g++ -g -shared extensiontest.cpp -o extensiontest.so $(INC)
+
+numpy_extension.so: numpy_extension.cpp
+	g++ -fPIC -g -shared numpy_extension.cpp -o numpy_extension.so $(INC)
+
--- test/c_extension/cuda_test.cu
+++ test/c_extension/cuda_test.cu
@@ -1,30 +1,12 @@
-# define DELLEXPORT extern "C"
-
 #include <stdio.h>
 #include <cuda.h>
 #include <cmath>
-#define THREADS_PER_BLOCK 1024
-// nvcc -Xcompiler -fPIC -shared -g cuda_test.cu -o cuda_test.so
+#include "cuda_test.cuh"
 using namespace std ;
-const double e = 2.7182818284590452353602874713527;
 
 
 
-__global__ void tanh_impl_Kernel(double * d_in, double * d_out, int size){
-	
-	int idx = threadIdx.x + blockIdx.x * blockDim.x;
-
-	if(idx < size){
-		// printf("%f\n", d_in[idx]);
-		double sinh = (1 - pow(e, (-2 * d_in[idx]))) / (2 * pow(e, -d_in[idx]));
-		double cosh = (1 + pow(e, (-2 * d_in[idx]))) / (2 * pow(e, -d_in[idx]));
-		d_out[idx] = sinh / cosh;
-		// d_out[idx] = ((1 - pow(e, (-2 * d_in[idx]))) / (2 * pow(e, -d_in[idx]))) / ((1 + pow(e, (-2 * d_in[idx]))) / (2 * pow(e, -d_in[idx])));
-	}
-}
-
-
-DELLEXPORT void tanh_impl(double * h_in, double * h_out, int arr_size){
+void tanh_impl(double * h_in, double * h_out, int arr_size){
 	
 	const long long int ARRAY_BYTES = arr_size * sizeof(double);
 	double *d_in, *d_out;
@@ -42,23 +24,3 @@ DELLEXPORT void tanh_impl(double * h_in, double * h_out, int arr_size){
 	cudaFree(d_in);
 	cudaFree(d_out);
 }
-
-
-// int main()
-// {
-	
-	
-// 	int cnt = 100000;
-
-// 	double * x = new double[cnt];
-
-// 	for(int i=0; i<cnt; ++i){
-// 		x[i] = 1.;
-// 	}
-// 	printf("a %f CPU.\n", x[0]);
-// 	double* a = tanh_impl(x, cnt);
-
-// 	printf("a %f CPU.\n", a[1]);
-
-// 	return 0;
-// }

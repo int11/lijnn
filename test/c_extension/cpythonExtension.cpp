0d85f471b6a8b6a2b57b5bd3952cd02f2cf83aaf
injea <injea1291@gmail.com>
2024-02-22 23:19:42 +0900

    python c extension test

--- test/c_extension/cpythonExtension.cpp
+++ test/c_extension/cpythonExtension.cpp
@@ -1,5 +1,9 @@
+#include <iostream>
 #include <cmath>
 #include <Python.h>
+#include <vector>
+
+using namespace std;
 
 const double e = 2.7182818284590452353602874713527;
 
@@ -15,34 +19,50 @@ double tanh_impl(double x) {
     return sinh_impl(x) / cosh_impl(x);
 }
 
-PyObject* _CPython_tanh_impl(PyObject*, PyObject* o) {
+PyObject* tanh_impl(PyObject*, PyObject* o) {
     double x = PyFloat_AsDouble(o);
     double tanh_x = tanh_impl(x);
     return PyFloat_FromDouble(tanh_x);
 }
 
-PyObject* _CPython_tanh_impl_point(PyObject*, PyObject* o) {
-    Py_ssize_t tot_len = PyList_Size(o);
-    PyObject* ret = PyList_New(tot_len);
+PyObject* tanh_impl_point(PyObject*, PyObject* o) {
     
-    for (int i = 0; i < tot_len; i++) {
-        double x = PyFloat_AsDouble(PyList_GetItem(o, i));
-        double tanh_x = std::tanh(x);
-        PyList_SetItem(ret, i, PyFloat_FromDouble(tanh_x));
+    PyObject *input, *output;
+    if (!PyArg_ParseTuple(o, "OO", &input, &output)) {
+        return NULL;
     }
+    // Py_ssize_t size = PyList_Size(input);
+    // PyObject* ret = PyList_New(size);
+    
+    // for (int i = 0; i < size; i++) {
+    //     double x = PyFloat_AsDouble(PyList_GetItem(input, i));
+    //     double tanh_x = std::tanh(x);
+    //     PyList_SetItem(ret, i, PyFloat_FromDouble(tanh_x));
+    // }
 
-    Py_INCREF(ret);
+    // Py_INCREF(ret);
 
-    return ret;
+    return output;
+}
+PyObject* my_function(PyObject* self, PyObject* args) {
+    PyObject* input;
+    if (!PyArg_ParseTuple(args, "O", &input)) {
+        return NULL;
+    }
+    
+    // Process the input object here
+    
+    Py_INCREF(input);
+    return input;
 }
 
 static PyMethodDef methods[] = {
     // The first property is the name exposed to Python, fast_tanh
     // The second is the C++ function with the implementation
     // METH_O means it takes a single PyObject argument
-    { "_CPython_tanh_impl", _CPython_tanh_impl, METH_O, "asdfasdftestete" },
-    { "_CPython_tanh_impl_point", _CPython_tanh_impl_point, METH_O, "asdfasdftestete" },
-
+    { "tanh_impl", tanh_impl, METH_O, "asdfasdftestete" },
+    { "tanh_impl_point", tanh_impl_point, METH_VARARGS, "asdfasdftestete" },
+{ "my_function", my_function, METH_VARARGS, "asdfasdftestete" },
     // Terminate the array with an object containing nulls.
     { nullptr, nullptr, 0, nullptr }
 };
--- test/c_extension/ctypes.cpp
+++ test/c_extension/ctypes.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#define DELLEXPORT extern "C"
 
 const double e = 2.7182818284590452353602874713527;
 
@@ -10,15 +11,15 @@ double cosh_impl(double x) {
     return (1 + pow(e, (-2 * x))) / (2 * pow(e, -x));
 }
 
-double tanh_impl(double x) {
+double test_tanh_impl(double x) {
     return sinh_impl(x) / cosh_impl(x);
 }
 
-extern "C" double ctypes_tanh_impl(double x){
-    return tanh_impl(x);
+DELLEXPORT double tanh_impl(double x){
+    return std::tanh(x);
 }
 
-extern "C" double* ctypes_tanh_impl_point(double* x, int cnt){
+DELLEXPORT double* tanh_impl_point(double* x, int cnt){
     double *result = new double[cnt];
     for (int i=0; i< cnt; i++){
         result[i] = std::tanh(x[i]);
